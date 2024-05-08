29e083745d2001604f64152022c0b38d8e9e5b50
fase99 <89035508+fase99@users.noreply.github.com>
2024-05-08 19:02:17 -0400

    cambios

--- api/index.js
+++ api/index.js
@@ -1,26 +1,18 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+
+//para importar los archivos html
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+
 
 const app = express()
 const port = 3000
 
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
-app.use(express.json({
-    type: "*/*"
-}))
-app.use(cors())
 
-app.get('/prueba', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.post('/login', (req, res) => {
-    console.log("me esta llegando algo");
-})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
