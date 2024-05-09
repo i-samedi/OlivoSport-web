5c2261f713e62c169087807e032360ffbb09cf9b
fase99 <89035508+fase99@users.noreply.github.com>
2024-05-08 20:18:43 -0400

    rutas
    se adhieren rutas de register y login, aun no son funcionales

--- api/index.js
+++ api/index.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const cors = require('cors');
+import express from "express"
+import cors from "cors";
 
 //para importar los archivos html
-import path from 'path';
 import { fileURLToPath } from 'url';
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import path, { dirname } from 'path';
 
-
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express()
 const port = 3000
 
 
+app.use(express.static('../web'));
+
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname+'/web/login.html'))
+})
+
+app.get("/register", (req,res) => {
+    res.sendFile(path.join(__dirname+'/web/register.html'))
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
