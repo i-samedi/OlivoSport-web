d2c48e5c6ebb593e4961978d476380aa7c64a2bf
fase99 <89035508+fase99@users.noreply.github.com>
2024-05-13 02:16:18 -0400

    configuracion de login

--- api/index.js
+++ api/index.js
@@ -10,12 +10,22 @@ const __dirname = dirname(__filename);
 const app = express()
 const port = 3000
 
+//middlewares
+app.use(express.json()); //entender formato json
+app.use(express.urlencoded({extended: false})); //para entender los datos que llegan desde un formulario
 
 app.use(express.static(__dirname + '/../web'))
 
 
+
+//Rutas de la API
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname +  '/web/index.html'))
+})
+
+app.post("/register", (req,res)=>{
+    console.log(req.body)
+    res.send('recived');
 })
 
 app.listen(port, () => {
