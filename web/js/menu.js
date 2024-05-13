1a9587ac100ae37657f5b338c9027f6643e40094
voltning <victor.rayo@mail.udp.cl>
2024-05-13 16:54:35 -0400

    Dashboard constante y funciones
    Se implementaron vinculos a los botones y tambien una funcion para cargar el contenido de los respectivos botones sin alterar a la sidebar.

--- web/js/menu.js
+++ web/js/menu.js
@@ -1,7 +1,6 @@
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.querySelector('#menu-btn');
 const closeBtn = document.querySelector('#close-btn');
-
 const themeToggler = document.querySelector('.theme-toggler');
 
 //show sidebar
@@ -19,3 +18,21 @@ themeToggler.addEventListener('click', () => {
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 
 })
+
+//cargar contenido al hacer clic en los enlaces del sidebar sin afectarla
+document.querySelectorAll('.sidebar a').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const href = this.getAttribute('href');
+        cargarContenido(href);
+    });
+});
+//Funcion cargar contenido 
+function cargarContenido(href) {
+    fetch(href)
+        .then(response => response.text())
+        .then(html => {
+            document.querySelector('.right').innerHTML = html;
+        })
+        .catch(error => console.error('Error al cargar contenido: ', error));
+}
