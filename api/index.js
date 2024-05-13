22039e2a040900497218bc9e28549f442334d3fe
fase99 <89035508+fase99@users.noreply.github.com>
2024-05-13 18:18:32 -0400

    bug json arreglado

--- api/index.js
+++ api/index.js
@@ -1,50 +1,47 @@
-import express, { Router } from "express"
+import express, { Router } from "express";
 
-const app = express()
-const port = 3000
-
+const app = express();
+const port = 3000;
 
 //para importar los archivos html
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 
-import session from 'express-session';
-
+import session from "express-session";
 
 //config
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-app.use(express.static(__dirname + '/../web'))
 
-/* app.set('../web', path.join(__dirname +'/../web'))
-app.set('view engine', 'ejs');
- */
 //middlewares
 app.use(express.json()); //entender formato json
-app.use(express.urlencoded({extended: false})); //para entender los datos que llegan desde un formulario
-/* app.use(session({
-    secret: 'olivosKey',
+app.use(express.urlencoded({ extended: true })); //para entender los datos que llegan desde un formulario
+app.use(express.static(__dirname + "/../web"));
+app.use(
+  session({
+    secret: "olivosKey",
     resave: false,
-   saveUninitialized: false
-})) */
-
+    saveUninitialized: false,
+  })
+);
 
 //Rutas de la API
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname +  '/web/index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/web/index.html"));
+});
 
-app.post("/register", (req,res)=>{
-    console.log(req.body)
-    //req.session.my_variable = 'hello world';
-    res.send("recived")
-})
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  //req.session.my_variable = 'hello world';
+  res.send("recived");
+});
 
 /* app.get('/profile', (req,res)=>{
     res.render('profile');
 })
-
  */
+
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
