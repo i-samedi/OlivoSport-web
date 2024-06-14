a87c5d58ca3929179716f0e545d7e111fcb3aa8d
Pipeemendez <94472095+Pipeemendez@users.noreply.github.com>
2024-06-13 22:21:29 -0400

    nuevo mongodb.js

--- api/mongodb.js
+++ api/mongodb.js
@@ -6,29 +6,4 @@ mongoose.connect("mongodb+srv://pipemendez:rn0pWgRZJk4FJ9T7@olivossport.exmftev.
     console.log("Conexion fallida con MongoDB")
 })
 
-const LoginSchema = new mongoose.Schema({
-    rut: { 
-        type: String, 
-        required: true 
-    },
-    password: {
-        type: String, 
-        required: true 
-    },
-    nombre: {
-        type: String,
-        required: true
-    },
-    apellido: {
-        type: String,
-        required: true
-    },
-    tipo_de_usuario: {
-        type: String,
-        required: true
-    }
-});
-
-mongoose.model("usuarios", LoginSchema);
-
 export default mongoose;
