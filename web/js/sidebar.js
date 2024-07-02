cf8e0789b0be427bdc822b7dd53de3848db016bc
Lord Samedi <ignacio.mediaz@gmail.com>
2024-07-02 15:20:16 -0400

    corrección sidebar.js

--- web/js/sidebar.js
+++ web/js/sidebar.js
@@ -1,26 +1,39 @@
-$(".sidebar ul li").on('click', function () {
-    $(".sidebar ul li.active").removeClass('active');
-    $(this).addClass('active');
-});
-
-$('.open-btn').on('click', function () {
-    $('.sidebar').addClass('active');
-});
-
-$('.close-btn').on('click', function () {
-    $('.sidebar').removeClass('active');
-});
-
-// Nuevo código para cerrar la sidebar al hacer clic fuera de ella
-$(document).on('click', function (e) {
-    if ($(window).width() <= 767) {
-        var sidebar = $('.sidebar');
-        var openBtn = $('.open-btn');
-        
-        // Si el clic no fue dentro de la sidebar ni en el botón de abrir
-        if (!sidebar.is(e.target) && sidebar.has(e.target).length === 0 &&
-            !openBtn.is(e.target) && openBtn.has(e.target).length === 0) {
-            sidebar.removeClass('active');
-        }
+$(document).ready(function() {
+    function setActiveLink() {
+        var currentPath = window.location.pathname;
+        $(".sidebar ul li").removeClass('active');
+        $(".sidebar ul li a").each(function() {
+            var link = $(this).attr('href');
+            if (currentPath.startsWith(link) && link !== '/') {
+                $(this).parent('li').addClass('active');
+            }
+        });
     }
+
+    setActiveLink();
+
+    $(".sidebar ul li").on('click', function () {
+        $(".sidebar ul li.active").removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.open-btn').on('click', function () {
+        $('.sidebar').addClass('active');
+    });
+
+    $('.close-btn').on('click', function () {
+        $('.sidebar').removeClass('active');
+    });
+
+    $(document).on('click', function (e) {
+        if ($(window).width() <= 767) {
+            var sidebar = $('.sidebar');
+            var openBtn = $('.open-btn');
+            
+            if (!sidebar.is(e.target) && sidebar.has(e.target).length === 0 &&
+                !openBtn.is(e.target) && openBtn.has(e.target).length === 0) {
+                sidebar.removeClass('active');
+            }
+        }
+    });
 });
